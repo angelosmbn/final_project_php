65810a7fbbd94f7d84f5e3801024a64f3d45a4f2
angelosmbn <angelosimbulan16@gmail.com>
2023-05-30 12:22:26 +0800

    s

--- success.php
+++ success.php
@@ -30,55 +30,47 @@
 <body>
     <?php
         if ($_SERVER["REQUEST_METHOD"] == "POST") {
-            $firstName = $_POST['firstName'];
-            $lastName = $_POST['lastName'];
-            $age = $_POST['age'];
-            $gender = $_POST['gender'];
-            $telephone = $_POST['telephone'];
+            // ...existing code...
 
-            if (substr($telephone, 0, 1) === '0') {
-                $telephone = '63' . substr($telephone, 1);
+            $conn = new mysqli('localhost', 'root', '', 'check_up');
+            if ($conn->connect_error) {
+                echo '<div class="error-message">
+                    Account NOT successfully created! Please
+                    <a href="signup.php">Try Again</a>
+                </div>';
+
+                die("Connection failed: ". $conn->connect_error);
+            } else {
+                // Check if the email is already taken
+                $emailQuery = "SELECT email FROM users WHERE email = ?";
+                $emailStmt = $conn->prepare($emailQuery);
+                $emailStmt->bind_param("s", $email);
+                $emailStmt->execute();
+                $emailResult = $emailStmt->get_result();
+
+                if ($emailResult->num_rows > 0) {
+                    // Email is already taken, display error message and redirect back to the signup form
+                    echo '<div class="error-message">
+                        Email is already taken. Please choose a different email.
+                    </div>';
+                    $emailStmt->close();
+                    $conn->close();
+                } else {
+                    // Email is not taken, proceed with account creation
+                    $stmt = $conn->prepare("INSERT INTO users(first_name, last_name, age, gender, phone_number, email, street, city, state, postal, country, role, specialization, license_number ,password)
+                        VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
+                    $stmt->bind_param("ssisissssisssss", $firstName, $lastName, $age, $gender, $telephone, $email, $street, $city, $state, $zip, $country, $role, $specializationsString, $licensesString, $password);
+                    $stmt->execute();
+                    echo '<div class="success-message">
+                        Account successfully created! Please
+                        <a href="login.php">Sign in</a>
+                    </div>';
+                    $stmt->close();
+                    $conn->close();
+                }
             }
-
-            $email = $_POST['email'];
-            $street = $_POST['street'];
-            $city = $_POST['city'];
-            $state = $_POST['state'];
-            $zip = $_POST['postal'];
-            $country = $_POST['country'];
-            $password = $_POST['password'];
-            $confirmPassword = $_POST['confirmPassword'];
-
-            $role = isset($_POST['role']) ? $_POST['role'] : '';
-            $specializations = isset($_POST['specialization']) ? $_POST['specialization'] : [];
-            $licenses = isset($_POST['license']) ? $_POST['license'] : [];
-
-            $specializationsString = implode(', ', $specializations);
-            $licensesString = implode(', ', $licenses);
-        }
-    
-        $conn = new mysqli('localhost', 'root', '', 'check_up');
-        if ($conn->connect_error) {
-            echo '<div class="success-message">
-                Account NOT successfully created! Please
-                <a href="login.php">Try Again</a>
-            </div>';
-
-            die("Connection failed: ". $conn->connect_error);
-        }
-        else{
-            $stmt = $conn->prepare("insert into users(first_name, last_name, age, gender, phone_number, email, street, city, state, postal, country, role, specialization, license_number ,password)
-                values(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
-            $stmt->bind_param("ssisissssisssss", $firstName, $lastName, $age, $gender, $telephone, $email, $street, $city, $state, $zip, $country, $role, $specializationsString, $licensesString, $password);
-            $stmt->execute();
-            echo '<div class="success-message">
-                Account successfully created! Please
-                <a href="login.php">Sign in</a>
-            </div>';
-            $stmt->close();
-            $conn->close();
         }
     ?>
-
 </body>
+
 </html>
