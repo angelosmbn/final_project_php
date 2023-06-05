d5435e63eb9bd010faaad096037fd15be148b2b8
angelosmbn <angelosimbulan16@gmail.com>
2023-06-05 22:52:08 +0800

    search filter

--- show_doctors.php
+++ show_doctors.php
@@ -27,9 +27,10 @@
       <h2>Sidebar</h2>
       <form action="Patient.php" method="post">
         <div class="search-bar">
-          <input type="text" placeholder="Search..." />
+          <input type="text" name="search" placeholder="Search..." />
         </div>
-        <select>
+        <select name="specialization">
+          <option value="">Filter Specialization</option>
           <option value="Cardiologist">Cardiologist</option>
           <option value="Dermatologist">Dermatologist</option>
           <option value="Endocrinologist">Endocrinologist</option>
@@ -40,51 +41,71 @@
           <option value="Psychiatrist">Psychiatrist</option>
           <option value="Surgeon">Surgeon</option>
         </select>
-
-        <input type="submit" />
+        <button type="submit">Filter</button>
       </form>
+
     </div>
 
-<!-- User Cards -->
-<div class="content">
-  <?php
-  // Establish the database connection
-  $conn = new mysqli('localhost', 'root', 'final123', 'check_up');
-
-  // Check for connection errors
-  if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-  }
-
-  // Fetch data from the doctor table
-  $sql = "SELECT firstName, lastName, specialization, picture FROM doctor";
-  $result = $conn->query($sql);
-
-  // Check if there are any rows in the result
-  if ($result->num_rows > 0) {
-    // Loop through each row and create a card
-    while ($row = $result->fetch_assoc()) {
-      ?>
-      <div class="card">
-        <img class="profile-image" src="<?php echo $row['picture']; ?>" alt="Profile Image">
-        <h2 class="text-title"><?php echo "Dr." . $row['firstName'] . ' ' . $row['lastName']; ?></h2>
-        <p class="text-body">Specialization: <?php echo $row['specialization']; ?></p>
-        <div class="info-card">
-          <h3>Additional Information</h3>
-          <p>.</p>
-        </div>
-        <a href="#" class="card-button">Book Now</a>
-      </div>
+    <!-- User Cards -->
+    <div class="content">
       <?php
-    }
-  } else {
-    echo "No doctors found.";
-  }
+      // Establish the database connection
+      $conn = new mysqli('localhost', 'root', 'final123', 'check_up');
 
-  // Close the database connection.
-  $conn->close();
-  ?>
-</div>
+      // Check for connection errors
+      if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+      }
+
+      // Fetch data from the doctor table
+      $sql = "SELECT firstName, lastName, specialization, picture FROM doctor";
+      
+      // Check if search input is provided
+      if (isset($_POST['search']) && !empty($_POST['search'])) {
+        $search = $_POST['search'];
+        // Add search filter to the SQL query
+        $sql .= " WHERE firstName LIKE '%$search%' OR lastName LIKE '%$search%'";
+      }
+
+      // Check if specialization is selected
+      if (isset($_POST['specialization']) && !empty($_POST['specialization'])) {
+        $specialization = $_POST['specialization'];
+        // Add specialization filter to the SQL query
+        if (strpos($sql, 'WHERE') !== false) {
+          $sql .= " AND specialization = '$specialization'";
+        } else {
+          $sql .= " WHERE specialization = '$specialization'";
+        }
+      }
+
+      $result = $conn->query($sql);
+
+      // Check if there are any rows in the result
+      if ($result->num_rows > 0) {
+        // Loop through each row and create a card
+        while ($row = $result->fetch_assoc()) {
+          ?>
+          <div class="card">
+            <img class="profile-image" src="<?php echo $row['picture']; ?>" alt="Profile Image">
+            <h2 class="text-title"><?php echo "Dr." . $row['firstName'] . ' ' . $row['lastName']; ?></h2>
+            <p class="text-body">Specialization: <?php echo $row['specialization']; ?></p>
+            <div class="info-card">
+              <h3>Additional Information</h3>
+              <p>.</p>
+            </div>
+            <a href="#" class="card-button">Book Now</a>
+          </div>
+          <?php
+        }
+      } else {
+        echo "No doctors found.";
+      }
+
+      // Close the database connection.
+      $conn->close();
+      ?>
+    </div>
+
 
 </body>
 </html>
